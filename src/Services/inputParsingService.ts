8657af80297d075244b7a8e863d47983daee11ed
Josh Hart <jhart.guitar@gmail.com>
2020-07-05 17:53:12 +0100

    begin implementing class parsing service

--- src/Services/inputParsingService.ts
+++ src/Services/inputParsingService.ts
@@ -1,11 +1,31 @@
-import { Input } from "@material-ui/core"
-
 export default class InputParsingService {
   static parseInputString(input: string) {
-    return this.removeBraces(input)
+    input = this.removeBeforeBrace(input)
+    input = this.removeBrace(input)
+    input = this.removeBeforeBrace(input)
+    input = this.removeBraces(input)
+    const lines = this.splitLines(input)
+    this.trimAllStrings(lines)
+    return lines
   }
 
   static removeBraces(input: string) {
     return input.replace(/{/g, '').replace(/}/g, '')
+  }
+
+  static removeBeforeBrace(input: string) {
+    return input.substring(input.indexOf('{'))
+  }
+
+  static removeBrace(input: string) {
+    return input.replace('{', '')
+  }
+
+  static splitLines(input: string) {
+    return input.split('\n')
+  }
+
+  static trimAllStrings(input: string[]) {
+    return input.map(Function.prototype.call, String.prototype.trim)
   }
 }
--- src/__tests__/Services/inputParsingService.test.js
+++ src/__tests__/Services/inputParsingService.test.js
@@ -13,9 +13,37 @@ test('removes many braces', () => {
     expect(expected).toEqual(actual);
   });
 
+  test('removes one brace', () => {
+    const expected = "some {text}}"
+    const input = "{some {text}}"
+    const actual = inputParsingService.removeBrace(input);
+    expect(expected).toEqual(actual);
+  });
+
+test('removes before first brace', () => {
+    const input = "public class blah {some text}"
+    const expected = "{some text}"
+    const actual = inputParsingService.removeBeforeBrace(input);
+    expect(expected).toEqual(actual);
+  });
+
+test('removes before first brace when multiple braces', () => {
+    const input = "public class blah {some {text}}"
+    const expected = "{some {text}}"
+    const actual = inputParsingService.removeBeforeBrace(input);
+    expect(expected).toEqual(actual);
+  });
+
+  test('splits string by new lines', () => {
+    const input = "one \n two \n three"
+    const expected = ["one ", " two ", " three"]
+    const actual = inputParsingService.splitLines(input);
+    expect(expected).toEqual(actual);
+  });
+
 test('sanitizes input into array of strings', () => {
-    const expected = "some text a \r\nsome text b "
-    const input = "some text a {}\r\nsome text b {}"
+    const input = "public class {some text a \nsome text b }"
+    const expected = ["some text a ", "some text b "]
     const actual = inputParsingService.parseInputString(input);
     expect(expected).toEqual(actual);
   });
